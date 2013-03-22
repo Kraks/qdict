d96c71ff88f529981dc2c85d8b153ebb4224e9de
Kraks <kiss.kraks@gmail.com>
2013-03-22 23:08:46 +0800

    add function prototype

--- kstr.h
+++ kstr.h
@@ -10,3 +10,13 @@ struct s_kstr {
 
 typedef char *kstr;
 
+kstr __newKstr(const void *init, size_t initlen);
+kstr newEmptyKstr(void);
+kstr newKstr(const char *init);
+void updateKstrLen(kstr s);
+void clearKstr(kstr s);
+kstr dupKstr(const kstr s);
+void freeKstr(kstr s);
+int kstrlen(const kstr s);
+static kstr makeRoomForKstr(kstr s, size_t addlen);
+void dumpKstr(kstr s);
