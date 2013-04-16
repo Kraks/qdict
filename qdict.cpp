8aee33f32b8cf35151568f188eb9be356ccca0ea
Kraks <kiss.kraks@gmail.com>
2013-04-17 00:45:58 +0800

    fix debug code

--- qdict.cpp
+++ qdict.cpp
@@ -167,8 +167,7 @@ int main(int argc, char **argv)
 	else {
 		vector<string> v(argv+1, argv+argc);
 #ifdef DEBUG
-		for (vector<string>::iterator it = v.begin(); it != v.end(); it++)
-			cout << *it << endl;
+		dumpVector(v);
 #endif
 		praseArgs(v);
 	}
