41c2519975a2f85faca5534850388628fe1b5c37
Kraks <kiss.kraks@gmail.com>
2013-04-29 23:09:33 +0800

    using fstream instead of FILE, not test

--- network.cpp
+++ network.cpp
@@ -34,11 +34,19 @@ t_word_string queryFromNetwork(string word, t_word_string &w)
 #ifdef DEBUG
 	printf("DEBUG: queryFromNetwork() %lu bytes retrieved\n", (long)chunk.size);
 #endif
+	/*
 	FILE *fp;
 	fp = fopen("_debug_.xml", "w");
 	if (fp) {
 		fputs((char *)chunk.memory, fp);
 		fclose(fp);
+	}
+	*/
+	
+	fstream f(word+".xml");
+	if (f) {
+		f << (char *) chunk.memory;
+		f.close();
 	}
 	
 	w = resolveYoudaoXML((char *)chunk.memory, w);
